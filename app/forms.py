b672e80214a2a7e0d46374daa539bd41fa0431e8
temirkhan <temirkhan.samurkhanov@mail.ru>
2023-03-30 23:12:35 +0300

    fixed regular expression for filtering number from 1 to 30

--- app/forms.py
+++ app/forms.py
@@ -4,21 +4,14 @@ from wtforms.validators import Length, Regexp, ValidationError, DataRequired
 
 
 class GenerateForm(FlaskForm):
-    @staticmethod
-    def number_range_check(form, field):
-        if not ('0' < field.data < '33'):
-            raise ValidationError("Значение длины поля не более 32 символов")
-
     numbers = BooleanField('Цифры')
     lower_let = BooleanField('Прописные буквы')
     upper_let = BooleanField('Строчные буквы')
     spec_char = BooleanField('Спец. символы')
-    input_size = StringField(validators=[
-        Regexp(r'^[0-9]+$', message='Значение поля должно быть числом'),
-        Length(min=1, max=2),
-        number_range_check
-    ])
     submit = SubmitField("СГЕНЕРИРОВАТЬ")
+    input_size = StringField(validators=[
+        Regexp(r'^[1-2]{1}[0-9]?|[3]{1}[0]{1}|[1-9]{1}$', message='Значение поля должно быть числом от 1 до 30'),
+        Length(min=1, max=2)])
 
 
 class PasswordVerificationForm(FlaskForm):
